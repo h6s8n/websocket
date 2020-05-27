c76941948652c59bf0c9f679dd0bc88bf8aa6dc0
hasan <eehumm@gmail.com>
2020-05-27 11:12:12 +0430

    add ->string('name'); (table:role&permission)

--- database/migrations/2020_05_27_102706_create_permissions_table.php
+++ database/migrations/2020_05_27_102706_create_permissions_table.php
@@ -15,6 +15,7 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
+            $table->string('name'); //edit ,delete
             $table->timestamps();
         });
     }
