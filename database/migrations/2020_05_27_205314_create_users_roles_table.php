f405faa5ece5eb5ea1ba4abac0a03f59c81445ae
hasan <eehumm@gmail.com>
2020-05-27 21:10:20 +0430

    add relation & fix eror mysql foring key  in users_roles table

--- database/migrations/2020_05_27_205314_create_users_roles_table.php
+++ database/migrations/2020_05_27_205314_create_users_roles_table.php
@@ -14,9 +14,14 @@ class CreateUsersRolesTable extends Migration
     public function up()
     {
         Schema::create('users_roles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+
+            $table->primary(['user_id', 'role_id']);
+        }); 
     }
 
     /**
