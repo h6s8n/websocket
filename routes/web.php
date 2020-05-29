2bfa26681713f9a0c8749281ae262d3639b85536
hasan <eehumm@gmail.com>
2020-05-29 14:36:51 +0430

    php artisan make:provider PermissionsServiceProvider

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (\Illuminate\Http\Request $request) {
     $user = $request->user();
-    dump($user->hasRole('admin'));
+    dump($user->hasPermissionTo('add user'));
 
     // return new \Illuminate\Http\Response('hello', 200);
 });
