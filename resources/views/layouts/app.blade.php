72cdd31cbbbcc0edf1ae8f98557e005cd7a664cd
hasan <eehumm@gmail.com>
2020-05-30 21:05:34 +0430

    create role directive in permissionprovider and use it in bldae

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,11 @@
 <!--                         @if (Auth::user()->hasRole('admin'))
                             <li><a class="nav-link" href="">Admin panel</a></li>
                         @endif -->
+                        @role('admin')
+                            <li><a class="nav-link" href="">Admin panel</a></li>
+                        @endrole
+
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
