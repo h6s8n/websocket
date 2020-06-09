cb92a58d4fd29eb3d6063f3bd60cece6ae091d94
hasan <eehumm@gmail.com>
2020-06-09 18:57:11 +0430

    create migration and required column

--- database/migrations/2020_06_09_174837_create_verifications_table.php
+++ database/migrations/2020_06_09_174837_create_verifications_table.php
@@ -15,7 +15,28 @@ class CreateVerificationsTable extends Migration
     {
         Schema::create('verifications', function (Blueprint $table) {
             $table->id();
+            $table->string('identifier')->unique();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('name');
+            $table->string('family');
+            $table->string('national_card');
+            $table->string('phone_number');
+            $table->string('mobile_number');
+            $table->string('birthday_date');
+            $table->string('address');
+            $table->string('card_number');
+            $table->string('sheba');
+            $table->string('national_card_image_address');
+            $table->boolean('started')->default(false);
+            $table->boolean('approved')->default(false);
+            $table->boolean('finished')->default(false);
+            $table->softDeletes();
             $table->timestamps();
+
+            
+            
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
