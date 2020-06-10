2aad951af173a095a2e8d65eefba5d22dc7f1b87
hasan <eehumm@gmail.com>
2020-06-10 17:58:22 +0430

    add relation in model user &verification

--- app/Verification.php
+++ app/Verification.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Verification extends Model
 {
-    //
+
+	use SoftDeletes;
+    
+	protect $fillable =[
+		'name',
+		'family',
+		'national_card',
+		'phone_number',
+		'mobile_number',
+		'birthday_date',
+		'address',
+		'address',
+		'card_number',
+		'sheba',
+		'national_card_image_address',
+
+	];
+        public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
