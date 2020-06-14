defa8763ad0e98d6f9f66be04f2b2ea1bed2bcaa
hasan <eehumm@gmail.com>
2020-06-14 16:58:53 +0430

    make individual verification routs

--- app/User.php
+++ app/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
     ];
 
-    public function verification {
+    public function verification() {
         return $this->hasOne('App\verification');
     }
 }
